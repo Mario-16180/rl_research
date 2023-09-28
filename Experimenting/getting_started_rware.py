299a1d2cb86d2dfd5326c4a54e4c3361c248c434
Mario-16180 <mario.alan.16180@gmail.com>
2023-09-28 19:31:24 +0400

    wandb

--- Experimenting/getting_started_rware.py
+++ Experimenting/getting_started_rware.py
@@ -1,12 +1,27 @@
 import imageio
 import os
 import gym
-
+import torch
+import wandb
 
 def save_frames_as_gif(frames, path='/home/mario.cantero/Documents/Research/rl_research/SavedRenders', filename='gym_animation.gif'):
     imageio.mimwrite(os.path.join(path, filename), frames, duration=20, format='gif')
 
 if __name__ == '__main__':
+
+    # start a new wandb run to track this script
+    wandb.init(
+    # set the wandb project where this run will be logged
+    project="rl_research_mbzuai",
+    
+    # track hyperparameters and run metadata
+    config={
+    "learning_rate": 0.02,
+    "architecture": "CNN",
+    "dataset": "CIFAR-100",
+    "epochs": 10}
+    )
+
     env_name = "procgen:procgen-bossfight-v0"
     env = gym.make(env_name, render_mode="rgb_array")
     obs = env.reset()
