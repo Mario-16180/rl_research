af5fa632c3df94418f171687c4c52d701fc3b6d9
Mario-16180 <mario.alan.16180@gmail.com>
2023-10-03 23:58:03 +0400

    :sparkles: Complete architecture of the agent
    The architecture of the agent based on the IMPALA implementation is almost complete. I just need to implement a dynamic output of the network based on the action space.

---
+++ Experimenting/Models/IMPALA_CNN.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import gym
+
+class impala_cnn(nn.Module):
+    """ This model was used in the paper "IMPALA: Scalable Distributed Deep-RL with
+    Importance Weighted Actor-Learner Architectures" https://arxiv.org/abs/1802.01561 
+    I only omit the LSTM part of the architecture, as I don't need it for this research."""
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def build_network(self):
+        # First convolutional layer + pooling + block
+        self.conv_pool_1 = nn.Sequential(
+            nn.Conv2d(in_channels=4, out_channels=16, kernel_size=3, stride=1),
+            nn.MaxPool2d(stride=[2, 2], kernel_size=[3, 3])
+            )
+        self.block_1 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1),
+        )
+
+        # Second convolutional layer + pooling + block
+        self.conv_pool_2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1),
+            nn.MaxPool2d(stride=[2, 2], kernel_size=[3, 3])
+            )
+        self.block_2 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+        )
+
+        # Third convolutional layer + pooling + block
+        self.conv_pool_3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1),
+            nn.MaxPool2d(stride=[2, 2], kernel_size=[3, 3])
+            )
+        self.block_3 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1),
+        )
+
+        # Flattening and MLP
+        self.flatten_to_mlp = nn.Sequential(
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(800, 256),
+            nn.Softmax(),
+        )
+    
+    def forward(self, x):
+        # Stack of 4 images to float32 and normalized
+        x = x.float() / 255.0
+        # First convolutional layer + pooling + block
+        x = self.conv_pool_1(x)
+        x_saved = x
+        x = self.block_1(x) + x_saved
+        # Second convolutional layer + pooling + block
+        x = self.conv_pool_2(x)
+        x_saved = x
+        x = self.block_2(x) + x_saved
+        # Third convolutional layer + pooling + block
+        x = self.conv_pool_3(x)
+        x_saved = x
+        x = self.block_3(x) + x_saved
+        # Flattening and MLP
+        x = self.flatten_to_mlp(x)
+        return x
+    
+if __name__ == '__main__':
+    #env_name = "procgen:procgen-bossfight-v0"
+    #env = gym.make(env_name)
+    #obs = env.reset()
+
+    model = impala_cnn()
+    model.build_network()
+    #print(model)
+    x = torch.rand((10, 4, 64, 64))
+    print(model(x).shape)
+    
